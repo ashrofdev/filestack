779dcdcbfa161738cc3139bf1e49b583080c76c5
First Last <salmanashraf853@gmail.com>
2020-04-02 01:07:58 +0100

    improving ui

--- src/Components/MyCabinet/MyCabinets.js
+++ src/Components/MyCabinet/MyCabinets.js
@@ -38,3 +38,78 @@ const MyCabinet = ({user, openFolder, nav, fileType, upload}) => {
 };
 
 export default MyCabinet;
+
+
+
+
+// import React from 'react';
+
+// const files = [
+    
+    
+//     {
+//         name: 'test.mp3',
+//         type: 'audio/mp3',
+//         size: '40mb'
+//     }
+// ]
+// const Cabinet = ({user, fileType, upload}) => {
+//     console.log(user.val.image)
+//     const images = Object.values(user.val.image)
+//     const videos = Object.values(user.val.video)
+//     const music = Object.values(user.val.music)
+//     const docs = Object.values(user.val.docs)
+//     const others = Object.values(user.val.others)
+//     return (
+//         <div className="cabinet page">
+//             <h2>{fileType} files</h2>
+//             <div className="layout">
+//                 {
+//                     fileType === 'image'?
+//                     images.map(e=>{
+//                         return<div>
+//                                 <img src={e}/>
+//                         </div>      
+//                     }):
+//                     fileType === 'video'?
+//                     videos.map(e=>{
+//                         return<div>
+//                                 <video controls>
+//                                     <source src={e}/>
+//                                 </video>
+//                         </div>      
+//                     }):
+//                     fileType === 'music'?
+//                     music.map(e=>{
+//                         return<div>
+//                                 <audio>
+//                                     <source src={e}/>
+//                                 </audio>
+//                         </div>      
+//                     }):
+//                     fileType === 'docs'?
+//                     docs.map(e=>{
+//                         return<div>
+//                             <a href={e}>v</a>
+//                         </div>      
+//                     }):
+//                     fileType === 'others'?
+//                     others.map(e=>{
+//                         return<div>
+//                                 <a href={e}>v</a>
+//                         </div>      
+//                     }):null
+
+//                 }
+                
+//             </div>
+//             <div className="cta">
+//                 <h3>Upload a new file</h3>
+//                 <label className="cta-btn" for="upload">↑ Upload</label>
+//                 <input id="upload" style={{display: 'none'}}  onChange={(e)=>upload(e)} type="file"/>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Cabinet;
